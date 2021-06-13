9c28e4f694a9eabcabcf278699019ecb78e35dae
a-b-v-k <62746809+a-b-v-k@users.noreply.github.com>
2021-06-13 12:44:39 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("30 days 30 hour challenge")
+
+
+# In[2]:
+
+
+Hours = "thirty"
+print(Hours)
+
+
+# In[3]:
+
+
+Days = "Thirty days"
+print(Days[5])
+
+
+# In[23]:
+
+
+print(Days[5:])
+
+
+# In[8]:
+
+
+Challenge = "I will win"
+
+print(len(Challenge))
+
+
+# In[25]:
+
+
+print(challenge)
+
+
+# In[26]:
+
+
+a = "30 Days"
+b = "30 hours"
+c = a + ' ' + b
+print(c)
+
+
+# In[27]:
+
+
+text = "Thirty days and Thirty hours"
+x = text.casefold()
+print(x)
+
+
+# In[28]:
+
+
+x = text.capitalize()
+print(x)
+
+
+# In[29]:
+
+
+x = text.isalpha()
+print(x)
+
+
+# In[30]:
+
+
+x = text.find('d')
+print(x)
+
+
+# In[31]:
+
+
+x = text.isalnum()
+print(x)
+
+
+
+
+
