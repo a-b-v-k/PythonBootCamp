86d33f274bdb9acd2815486806e34de338b3ce2e
a-b-v-k <vijaykrishnaabv@gmail.com>
2021-06-15 10:31:59 +0530

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,22 @@
+a = b = c = 30
+
+print(a/10)
+print(b/50)
+print(c+60)
+
+s = "Hello"
+
+s = s[:2] + 'G' + s[3:]
+
+print(s)
+
+x = int()
+y = float()
+
+x = 10
+y = 12.5
+
+x = float(x)
+y = int(y)
+
+print('x = ' + str(x) + ' y = ' + str(y))
