ec30ce168be001c8f95af73ae71e09d120edd108
a-b-v-k <vijaykrishnaabv@gmail.com>
2021-06-24 10:09:43 +0530

    Day 12 task

---
+++ day12.py
@@ -0,0 +1,7 @@
+file_obj = open("30 days 30 hour operations", 'w')
+file_obj.write('I have completed 10 days successfully.')
+file_obj.close()
+
+f = open("30 days 30 hour operations", 'a')
+f.write("\nName: Vijay Krishna A B")
+f.close()
