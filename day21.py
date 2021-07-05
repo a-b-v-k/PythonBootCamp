2a0bfb61946c927f29be4331b90bebaaa301763b
a-b-v-k <vijaykrishnaabv@gmail.com>
2021-07-05 09:39:45 +0530

    Day 21 task

--- day21.py
+++ day21.py
@@ -1,22 +1,19 @@
-import requests
-from bs4 import BeautifulSoup
-   
-URL = "http://www.values.com/inspirational-quotes"
-r = requests.get(URL)
-   
-soup = BeautifulSoup(r.content, 'html.parser')
-   
-quotes=[]  
-   
-table = soup.find('div', attrs = {'id':'all_quotes'}) 
-   
-for row in table.findAll('div', attrs = {'class':'col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top'}):
-    quote = {}
-    quote['theme'] = row.h5.text
-    quote['url'] = row.a['href']
-    quote['lines'] = row.img['alt'].split(" #")[0]
-    quotes.append(quote)
+def prog1(l1, l2):
+    print(list(zip(l1, l2)))
 
-for quote in quotes:
-    print('Theme:', quote['theme'])
-    print('Quote:', quote['lines'])
+def prog2(l):
+    print(list(zip(l, range(1, 9))))
+
+def prog3(l):
+    print(sorted(l))
+
+def prog4(l):
+    print(list(filter(lambda x : x%2 != 0, l)))
+
+if __name__ == '__main__':
+    l1 = [2, 7, 1, 9, 0]
+    l2 = [1, 2, 3, 4, 5]
+    prog1(l1, l2)
+    prog2([1, 6, 9, 4, 2, 8, 3, 7])
+    prog3(l1)
+    prog4(l1)
