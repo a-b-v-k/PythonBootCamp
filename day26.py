971d2ab32fa68cc794e88c2a5cadfce57a9438b2
a-b-v-k <vijaykrishnaabv@gmail.com>
2021-07-11 10:45:18 +0530

    Day 26 task

---
+++ day26.py
@@ -0,0 +1,12 @@
+l = [10, 20, 30, 40, 50]
+
+t = (2, 4, 6, 8, 10)
+
+
+print('Enumerate list')
+for i, j in enumerate(l):
+    print('Counter:', i, ' Value:',j)
+
+print('Enumerate tuple')
+for i, j in enumerate(t):
+    print('Counter:', i, ' Value:',j)
