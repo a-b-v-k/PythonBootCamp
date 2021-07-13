96fecc4dfb030fec1e2c75570a36108642d7fae9
a-b-v-k <vijaykrishnaabv@gmail.com>
2021-07-13 21:43:45 +0530

    Day 24 task

--- day24.py
+++ day24.py
@@ -4,7 +4,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="Vicky@2003",
+  password="*****",
   database="sample",
   buffered = True,
 )
